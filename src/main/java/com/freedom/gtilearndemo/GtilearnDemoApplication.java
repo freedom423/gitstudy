b021ae6243ae92f8b3db6ba81a53433a3076370c
freedom <1943664865@qq.com>
2022-08-02 15:42:24 +0800

    第一次提交信息

---
+++ src/main/java/com/freedom/gtilearndemo/GtilearnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.freedom.gtilearndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtilearnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtilearnDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freedom/gtilearndemo/GtilearnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freedom.gtilearndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtilearnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
