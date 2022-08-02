99bde9b77a6bed2e59caae7e4c75f4b8edaefd51
freedom <1943664865@qq.com>
2022-08-02 21:42:40 +0800

    hot-fix first

--- src/main/java/com/freedom/gtilearndemo/User.java
+++ src/main/java/com/freedom/gtilearndemo/User.java
@@ -1,8 +1,18 @@
 package com.freedom.gtilearndemo;
 
+import java.io.Serializable;
+
 /**
  * author: freedom
  * date：2022-08-02 15:44
  */
-public class User {
+
+public class User  implements Serializable {
+    int age;
+    int sex;
+    public User(){};
+    public User(int age,int sex){
+        this.age=age;
+        this.sex=sex;
+    }
 }
